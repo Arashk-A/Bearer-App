0b7a0ebb13c604a7c5e27f9f3d1de6c8063ea64a
zero <m.birang@gmail.com>
2023-04-09 01:09:10 +0330

    Initial Commit

---
+++ Bearer/BearerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BearerApp.swift
+//  Bearer
+//
+//  Created by zero on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct BearerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
